7fe13d680d2ea7952cdd3e335c92dbdc503f0f06
Dsky <dutian1990@gmail.com>
2021-01-18 18:57:25 +0800

    Add files via upload

---
+++ precache-manifest.002288ca207c497d30f9d81b9c7cab15.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be5541128d163795655011e83b49853",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/info@main/index.html"
+  },
+  {
+    "revision": "e571717a00ba00474c53",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/info@main/static/js/2.fbc59ff7.chunk.js"
+  },
+  {
+    "revision": "48dc2a0f18dfdbbcdcf81b94d8a3e1da",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/info@main/static/js/2.fbc59ff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4824bc67ce0494a8c37",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/info@main/static/js/main.bdf748f8.chunk.js"
+  },
+  {
+    "revision": "2339fdf513cbe150d74a",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/info@main/static/js/runtime-main.e526c010.js"
+  },
+  {
+    "revision": "73dabb371ddce7c640334fe0904dcddf",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/info@main/static/media/eth.73dabb37.png"
+  },
+  {
+    "revision": "5827780d34d7c141d5e763fd7b596313",
+    "url": "https://cdn.jsdelivr.net/gh/chickenswap/info@main/static/media/logo.5827780d.svg"
+  }
+]);
